a25b83310158e27ce1793d5fcb19180d7171082e
navaponk <66858475+navaponk@users.noreply.github.com>
2023-12-12 19:35:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ function updateCountdown() {
   const now = new Date().getTime();
   const timeLeft = targetDate - now;
 
-
+  // Check if the timer has reached zero
   if (timeLeft <= 0 && !showWatchNowBox) {
-  
+    // Hide the countdown container
     document.getElementById("countdownContainer").style.display = "none";
 
-   
+    // Show the "Happy Anniversary" text
     document.getElementById("anniversaryText").style.display = "block";
 
-   
+    // Set a timeout to hide the "Happy Anniversary" text and show the "Watch Now" box after 10 seconds
     setTimeout(hideAnniversaryTextAndShowWatchNowBox, 10000);
 
-    
+    // Set the flag to prevent further execution of this block
     showWatchNowBox = true;
   }
 
   if (timeLeft <= 0) {
- 
+    // Stop further execution of the function
     clearInterval(timerInterval);
   } else {
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
@@ -37,19 +37,20 @@ function updateCountdown() {
 }
 
 function hideAnniversaryTextAndShowWatchNowBox() {
-
+  // Hide the "Happy Anniversary" text
   document.getElementById("anniversaryText").style.display = "none";
 
+  // Show the "Watch Now" box
   document.getElementById("watchNowBox").style.display = "block";
 }
 
 function redirectToVideo() {
- 
+  // Redirect the user to the YouTube video with autoplay and specific settings
   window.location.href = 'https://www.youtube.com/embed/Y_hdQXQfsNg?autoplay=1&controls=0&fullscreen=1';
 }
 
-
+// Initial call to set up the countdown
 updateCountdown();
 
-
+// Set up an interval to update the countdown every second
 const timerInterval = setInterval(updateCountdown, 1000);
